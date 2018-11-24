f31c1c7a7a3338807df8e926e959098143a9c85f
JonahGroendal <jone951@gmail.com>
2018-11-24 13:50:40 -0500

    removed console.log

--- test/RsaSha256Algorithm.js
+++ test/RsaSha256Algorithm.js
@@ -6,7 +6,7 @@ contract('RsaSha256Algorithm', (accounts) => {
   it("should verify a valid signature", async () => {
     let instance = await RsaSha256Algorithm.deployed();
     let result = await instance.verify(cert.signersPubKey, cert.tbsCertificate, cert.signature);
-    console.log(result)
+    
     assert.isTrue(result, "Validation returned false");
   })
   it("should fail to verify an invalid signature", async () => {
